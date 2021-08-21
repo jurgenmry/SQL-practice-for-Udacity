8a94001e01a03d78608a06bdf1aac8b3c44c3c61
jurgenmry <jurgenmeneses@gmail.com>
2021-08-21 12:38:12 +0300

    fixing code demo2

--- demo2.py
+++ demo2.py
@@ -1,11 +1,11 @@
 import psycopg2
 
-cnn =  psycopg2.connect('dbname=exmaple user=postgres password=123')
+cnn =  psycopg2.connect('dbname=example user=postgres password=123')
 cursor = cnn.cursor()
 
-cursor.execute('DROP TABLE IF EXIST example;')
+#cursor.execute('DROP TABLE IF EXISTS table1;')
 
-cursor. execute('''
+cursor.execute('''
     CREATE TABLE table1(
         id INTEGER PRIMARY KEY,
         completed BOOLEAN NOT NULL DEFAULT false
@@ -13,8 +13,8 @@ cursor. execute('''
 ''')
 
 data = {
-    'id':1,
-    'completed': True,
+    'id':2,
+    'completed': False,
 }
 
 SQL = ('INSERT INTO table1 (id, completed) VALUES (%(id)s, %(completed)s);')
